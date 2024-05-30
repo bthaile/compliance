5af76f3325599b06f0b26416446dc62f53895a5d
Tom Haile <bradford.haile@gmail.com>
2024-05-29 21:24:41 -0500

    Update compliance.tsx

--- src/pages/compliance.tsx
+++ src/pages/compliance.tsx
@@ -377,7 +377,7 @@ const ComplianceCharts = () => {
                                 <MenuItem value="goal_status">Goal Status</MenuItem>
                                 <MenuItem value="lending_trends">Lending Trends</MenuItem>
                                 <MenuItem value="census_tracks">Census Tracks</MenuItem>
-                                <MenuItem value="loan_portfolio">Loan Portfolio</MenuItem>
+                              
                             </Select>
                         </Toolbar>
                     </AppBar>
